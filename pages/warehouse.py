e0104b2f8acc12ec5d3d8fd3659c39c3266d171a
Shriwayanta <shrithissidee@gmail.com>
2026-04-25 20:05:09 +0530

    Update warehouse.py
    Co-Authored-By: Harshal Pandit Nanekar <harshalnanekar69@gmail.com>

--- pages/warehouse.py
+++ pages/warehouse.py
@@ -1,3 +1,7 @@
+                        #Name: Shriwayanta Maiti, PRN: 20240802169
+                        #Name: Harshal Nanekar, PRN: 20240802163
+
+
 """
 Data Warehouse page – schema explorer and raw data viewer.
 Demonstrates Star Schema concept.
